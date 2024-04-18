bbbc547e3b4a622cdcaad20c5544f515c32d4af1
Yunyop <Yunyop.pyc@gmail.com>
2024-04-18 18:07:44 +0800

    4.3修改启动日志，增加/member，修改logback日志

--- Member/src/main/java/com/yun/train/config/MemberApplication.java
+++ Member/src/main/java/com/yun/train/config/MemberApplication.java
@@ -1,15 +1,22 @@
 package com.yun.train.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @ComponentScan("com.yun")
 public class MemberApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MemberApplication.class, args);
-	}
+	private static final Logger LOGGER = LoggerFactory.getLogger(MemberApplication.class);
 
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(MemberApplication.class);
+		Environment env = app.run(args).getEnvironment();
+		LOGGER.info("启动成功");
+		LOGGER.info("地址：\thttp://127.0.0.1:{}/hello", env.getProperty("server.port"));
+	}
 }
