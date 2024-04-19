b87a65a9f7844644caa80ee3c06be9639ccbe8a2
Yunyop <Yunyop.pyc@gmail.com>
2024-04-19 14:58:12 +0800

    4.9增加gateway模块

---
+++ gateway/src/main/java/com/yun/train/gateway/config/GatewayApplication.java
@@ -0,0 +1,22 @@
+package com.yun.train.gateway.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
+
+@SpringBootApplication
+@ComponentScan("com.yun")
+public class GatewayApplication {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(GatewayApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(GatewayApplication.class);
+		Environment env = app.run(args).getEnvironment();
+		LOGGER.info("启动成功");
+		LOGGER.info("网关地址：\thttp://127.0.0.1:{}/hello", env.getProperty("server.port"));
+	}
+}
