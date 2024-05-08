a8cfc2ee113e0ff7e2084d42f8dab32644bf2b26
Yunyop <Yunyop.pyc@gmail.com>
2024-05-08 14:51:39 +0800

    4.19集成校验框架Validation

--- member/src/main/java/com/yun/train/controller/MemberController.java
+++ member/src/main/java/com/yun/train/controller/MemberController.java
@@ -4,6 +4,7 @@ import com.yun.train.req.MemberRegisterReq;
 import com.yun.train.resp.CommonResp;
 import com.yun.train.service.MemberService;
 import jakarta.annotation.Resource;
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,7 +24,7 @@ public class MemberController {
         return new CommonResp<>(count);
     }
     @PostMapping("/register")
-    public CommonResp<Long> register(MemberRegisterReq memberRegisterReq) {
+    public CommonResp<Long> register(@Valid MemberRegisterReq memberRegisterReq) {
         long register = memberService.register(memberRegisterReq);
         return new CommonResp<>(register);
     }
