8ee1790303b23fbbee6dbd241758152d1461c50b
Yunyop <Yunyop.pyc@gmail.com>
2024-05-15 15:02:28 +0800

    5.9修改getway 解决跨域问题

--- member/src/main/java/com/yun/train/controller/MemberController.java
+++ member/src/main/java/com/yun/train/controller/MemberController.java
@@ -8,10 +8,7 @@ import com.yun.train.resp.MemberLoginResp;
 import com.yun.train.service.MemberService;
 import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/member")
@@ -32,7 +29,7 @@ public class MemberController {
         return new CommonResp<>(register);
     }
     @PostMapping("/send-code")
-    public CommonResp<Long> sendCode(@Valid MemberSendCodeReq req) {
+    public CommonResp<Long> sendCode(@Valid @RequestBody MemberSendCodeReq req) {
         memberService.sendCode(req);
         return new CommonResp<>();
     }
