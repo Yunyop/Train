4dfd0116a6a32bf1e12646b136237be56cdfdca7
Yunyop <Yunyop.pyc@gmail.com>
2024-05-20 21:59:29 +0800

    6.6vuex配合h5 session缓存，解决浏览器刷新丢失数据问题

--- web/src/store/index.js
+++ web/src/store/index.js
@@ -1,14 +1,16 @@
 import { createStore } from 'vuex'
-
+const MEMBER="MEMBER"
 export default createStore({
   state: {
-    member:{}
+    // 避免空指针异常
+    member:window.SessionStorage.get(MEMBER)||{}
   },
   getters: {
   },
   mutations: {
     setMember (state, member) {
-      state.member=member
+      state.member=member;
+      window.SessionStorage.set(MEMBER,member);
     }
   },
   actions: {
