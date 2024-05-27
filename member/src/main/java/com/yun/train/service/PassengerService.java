adb8fe9b9382ff5c6afbcb7db8f274cef7ffd343
Yunyop <Yunyop.pyc@gmail.com>
2024-05-27 16:42:12 +0800

    7.3增加乘车人新增接口

---
+++ member/src/main/java/com/yun/train/service/PassengerService.java
@@ -0,0 +1,24 @@
+package com.yun.train.service;
+
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.date.DateTime;
+import com.yun.train.domain.Passenger;
+import com.yun.train.mapper.PassengerMapper;
+import com.yun.train.req.PassengerSaveReq;
+import com.yun.train.util.SnowUtil;
+import jakarta.annotation.Resource;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PassengerService {
+    @Resource
+    private PassengerMapper passengerMapper;
+    public void save(PassengerSaveReq req){
+        DateTime now = DateTime.now();
+        Passenger passenger = BeanUtil.copyProperties(req, Passenger.class);
+        passenger.setId(SnowUtil.getSnowflakeNextId());
+        passenger.setCreateTime(now);
+        passenger.setUpdateTime(now);
+        passengerMapper.insert(passenger);
+    }
+}
