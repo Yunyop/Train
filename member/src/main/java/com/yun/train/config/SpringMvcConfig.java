28ddae2d4d75f3ffe9a67da74583ec59356b8925
Yunyop <Yunyop.pyc@gmail.com>
2024-05-28 16:19:25 +0800

    7.5增加日志拦截器，统一打印流水号

--- member/src/main/java/com/yun/train/config/SpringMvcConfig.java
+++ member/src/main/java/com/yun/train/config/SpringMvcConfig.java
@@ -2,6 +2,7 @@ package com.yun.train.config;
 
 
 import com.yun.train.interceptor.MemberInterceptor;
+import com.yun.train.interceptor.LoginInterceptor;
 import jakarta.annotation.Resource;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -10,15 +11,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class SpringMvcConfig implements WebMvcConfigurer {
 
-//   @Resource
-//   LogInterceptor logInterceptor;
+    @Resource
+    LoginInterceptor loginInterceptor;
 
    @Resource
    MemberInterceptor memberInterceptor;
 
    @Override
    public void addInterceptors(InterceptorRegistry registry) {
-//       registry.addInterceptor(logInterceptor);
+       registry.addInterceptor(loginInterceptor);
 
        // 路径不要包含context-path
        registry.addInterceptor(memberInterceptor)
