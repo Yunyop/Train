0ab3d482fc4c92307939920b21cab6ddd0cfb811
Yunyop <Yunyop.pyc@gmail.com>
2024-06-03 16:16:28 +0800

    7.6增加乘车人管理页面，实现页面切换

---
+++ web/src/views/main/passenger.vue
@@ -0,0 +1,16 @@
+<script>
+import {defineComponent} from "vue";
+export default defineComponent({
+  setup(){
+    return
+  },
+});
+</script>
+
+<template>
+  <h1>乘车人管理页面</h1>
+</template>
+
+<style scoped>
+
+</style>
