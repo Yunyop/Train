96e24a233d83f39e28efc29a5f55211700b532a7
Yunyop <Yunyop.pyc@gmail.com>
2024-06-12 21:09:35 +0800

    7.8增加乘车人列表查询接口，只能查看当前会员自己添加的乘客

--- member/src/main/java/com/yun/train/controller/PassengerController.java
+++ member/src/main/java/com/yun/train/controller/PassengerController.java
@@ -1,14 +1,16 @@
 package com.yun.train.controller;
 
+import com.yun.train.context.LoginMemberContext;
+import com.yun.train.req.PassengerQueryReq;
 import com.yun.train.req.PassengerSaveReq;
 import com.yun.train.resp.CommonResp;
+import com.yun.train.resp.PassengerQueryResp;
 import com.yun.train.service.PassengerService;
 import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/passenger")
@@ -20,6 +22,12 @@ public class PassengerController {
     public CommonResp<Object> save(@Valid @RequestBody PassengerSaveReq req) {
         passengerService.save(req);
         return new CommonResp<>();
+    }
+    @GetMapping("/query-list")
+    public CommonResp<List<PassengerQueryResp>> queryList(@Valid PassengerQueryReq req) {
+        req.setMemberId(LoginMemberContext.getId());
+        List<PassengerQueryResp> list = passengerService.queryList(req);
+        return new CommonResp<>(list);
     }
 
 }
--- member/src/main/java/com/yun/train/service/PassengerService.java
+++ member/src/main/java/com/yun/train/service/PassengerService.java
@@ -2,13 +2,19 @@ package com.yun.train.service;
 
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.date.DateTime;
+import cn.hutool.core.util.ObjectUtil;
 import com.yun.train.context.LoginMemberContext;
 import com.yun.train.domain.Passenger;
+import com.yun.train.domain.PassengerExample;
 import com.yun.train.mapper.PassengerMapper;
+import com.yun.train.req.PassengerQueryReq;
 import com.yun.train.req.PassengerSaveReq;
+import com.yun.train.resp.PassengerQueryResp;
 import com.yun.train.util.SnowUtil;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PassengerService {
@@ -22,5 +28,14 @@ public class PassengerService {
         passenger.setCreateTime(now);
         passenger.setUpdateTime(now);
         passengerMapper.insert(passenger);
+    }
+    public List<PassengerQueryResp> queryList(PassengerQueryReq req){
+        PassengerExample passengerExample = new PassengerExample();
+        PassengerExample.Criteria criteria = passengerExample.createCriteria();
+        if (ObjectUtil.isNotNull(req.getMemberId())) {
+            criteria.andMemberIdEqualTo(req.getMemberId());
+        }
+        List<Passenger> passengerList = passengerMapper.selectByExample(passengerExample);
+        return BeanUtil.copyToList(passengerList, PassengerQueryResp.class);
     }
 }
