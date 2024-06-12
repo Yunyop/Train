ca09e8abddff74b28d33a7c2851ffeacfabce268
Yunyop <Yunyop.pyc@gmail.com>
2024-06-12 15:46:12 +0800

    7.7乘车人新增页面开发，增加【新增】按钮

--- web/src/views/main/passenger.vue
+++ web/src/views/main/passenger.vue
@@ -1,14 +1,31 @@
 <script>
-import {defineComponent} from "vue";
+import {defineComponent, ref} from "vue";
 export default defineComponent({
   setup(){
-    return
+    const visible=ref(false);
+    const showModal=()=>{
+      visible.value=true;
+    };
+    const handleOk=e=>{
+      console.log(e);
+      visible.value=false;
+    }
+    return{
+      visible,
+      showModal,
+      handleOk,
+    }
   },
 });
 </script>
 
 <template>
-  <h1>乘车人管理页面</h1>
+  <a-button type="primary" @click="showModal">新增</a-button>
+  <a-modal v-model:visible="visible" title="乘车人" @ok="handleOk">
+    <p>Some contents...</p>
+    <p>Some contents...</p>
+    <p>Some contents...</p>
+  </a-modal>
 </template>
 
 <style scoped>
