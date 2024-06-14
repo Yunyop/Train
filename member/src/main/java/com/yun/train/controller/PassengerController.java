88e29d46463a985e3e60afeab94c972d70f664b1
Yunyop <Yunyop.pyc@gmail.com>
2024-06-14 16:19:14 +0800

    7.9封装分页返回结果

--- member/src/main/java/com/yun/train/controller/PassengerController.java
+++ member/src/main/java/com/yun/train/controller/PassengerController.java
@@ -4,13 +4,12 @@ import com.yun.train.context.LoginMemberContext;
 import com.yun.train.req.PassengerQueryReq;
 import com.yun.train.req.PassengerSaveReq;
 import com.yun.train.resp.CommonResp;
+import com.yun.train.resp.PageResp;
 import com.yun.train.resp.PassengerQueryResp;
 import com.yun.train.service.PassengerService;
 import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/passenger")
@@ -24,9 +23,9 @@ public class PassengerController {
         return new CommonResp<>();
     }
     @GetMapping("/query-list")
-    public CommonResp<List<PassengerQueryResp>> queryList(@Valid PassengerQueryReq req) {
+    public CommonResp<PageResp<PassengerQueryResp>> queryList(@Valid PassengerQueryReq req) {
         req.setMemberId(LoginMemberContext.getId());
-        List<PassengerQueryResp> list = passengerService.queryList(req);
+        PageResp<PassengerQueryResp> list = passengerService.queryList(req);
         return new CommonResp<>(list);
     }
 
--- member/src/main/java/com/yun/train/service/PassengerService.java
+++ member/src/main/java/com/yun/train/service/PassengerService.java
@@ -4,21 +4,29 @@ import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.date.DateTime;
 import cn.hutool.core.util.ObjectUtil;
 import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.yun.train.context.LoginMemberContext;
 import com.yun.train.domain.Passenger;
 import com.yun.train.domain.PassengerExample;
 import com.yun.train.mapper.PassengerMapper;
 import com.yun.train.req.PassengerQueryReq;
 import com.yun.train.req.PassengerSaveReq;
+import com.yun.train.resp.PageResp;
 import com.yun.train.resp.PassengerQueryResp;
 import com.yun.train.util.SnowUtil;
 import jakarta.annotation.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class PassengerService {
+
+    private static final Logger LOGGER= LoggerFactory.getLogger(PassengerService.class);
+
+
     @Resource
     private PassengerMapper passengerMapper;
     public void save(PassengerSaveReq req){
@@ -30,14 +38,28 @@ public class PassengerService {
         passenger.setUpdateTime(now);
         passengerMapper.insert(passenger);
     }
-    public List<PassengerQueryResp> queryList(PassengerQueryReq req){
+    public PageResp<PassengerQueryResp> queryList(PassengerQueryReq req){
         PassengerExample passengerExample = new PassengerExample();
         PassengerExample.Criteria criteria = passengerExample.createCriteria();
         if (ObjectUtil.isNotNull(req.getMemberId())) {
             criteria.andMemberIdEqualTo(req.getMemberId());
         }
+        LOGGER.info("查询页码：{}",req.getPage());
+        LOGGER.info("每页条数：{}",req.getSize());
         PageHelper.startPage(req.getPage(),req.getSize());
         List<Passenger> passengerList = passengerMapper.selectByExample(passengerExample);
-        return BeanUtil.copyToList(passengerList, PassengerQueryResp.class);
+        PageInfo<Passenger> pageInfo = new PageInfo<>(passengerList);
+
+        LOGGER.info("总行数：{}",pageInfo.getTotal());
+        LOGGER.info("总页数：{}",pageInfo.getPages());
+
+        List<PassengerQueryResp> list = BeanUtil.copyToList(passengerList, PassengerQueryResp.class);
+
+        PageResp<PassengerQueryResp> pageResp = new PageResp<>();
+
+        pageResp.setList(list);
+
+        pageResp.setTotal(pageInfo.getTotal());
+        return pageResp;
     }
 }
