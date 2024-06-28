5c6976a2d027acaf2de930e251673ab2af741138
Yunyop <Yunyop.pyc@gmail.com>
2024-06-28 14:17:47 +0800

    7.11解决前后端Long类型精度丢失问题

--- member/src/main/java/com/yun/train/resp/PassengerQueryResp.java
+++ member/src/main/java/com/yun/train/resp/PassengerQueryResp.java
@@ -1,10 +1,13 @@
 package com.yun.train.resp;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 
 import java.util.Date;
 
 public class PassengerQueryResp {
+    @JsonSerialize(using = ToStringSerializer.class)
     private Long id;
 
     private String name;
