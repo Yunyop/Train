0ecb2af25d12738976ead32d50ede64aaafc3be2
Yunyop <Yunyop.pyc@gmail.com>
2024-07-04 18:30:36 +0800

    7.13乘车人删除功能开发

--- web/src/views/main/passenger.vue
+++ web/src/views/main/passenger.vue
@@ -16,6 +16,12 @@
       <template v-if="column.dataIndex==='operation'">
         <a-space>
           <a @click="onEdit(record)">编辑</a>
+          <a-popconfirm
+            title="删除后不可恢复，确认删除？"
+            @confirm="onDelete(record)"
+            ok-text="确认" cancel-text="取消">
+            <a style="color: red">删除</a>
+          </a-popconfirm>
         </a-space>
       </template>
     </template>
