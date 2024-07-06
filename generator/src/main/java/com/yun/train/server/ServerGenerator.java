10830435132bf9ba4ab40a59074b2d261aa6c517
Yunyop <Yunyop.pyc@gmail.com>
2024-07-06 15:47:23 +0800

    8.2集成dom4j。读取当前持久层的xml文件，得到表面和实体命

--- generator/src/main/java/com/yun/train/server/ServerGenerator.java
+++ generator/src/main/java/com/yun/train/server/ServerGenerator.java
@@ -1,6 +1,7 @@
 package com.yun.train.server;
 
 import org.dom4j.Document;
+import org.dom4j.DocumentException;
 import org.dom4j.Node;
 import org.dom4j.io.SAXReader;
 
@@ -16,6 +17,26 @@ public class ServerGenerator {
     }
     public static void main(String[] args) throws Exception{
 //        读取xml
+        String generatorPath = getGeneratorPath();
+
+
+//        读xml里面的节点
+
+        Document document = new SAXReader().read("generator/" + generatorPath);
+        Node table = document.selectSingleNode("//table");
+        System.out.println(table);
+        Node tableName = table.selectSingleNode("@tableName");
+        Node domainObject = table.selectSingleNode("@domainObjectName");
+        System.out.println(tableName.getText()+"/"+domainObject.getText());
+
+
+//        FreemarkerUtil.initConfig("test.ftl");
+//        Map<String, Object> data = new HashMap<>();
+//        data.put("domain","Test");
+//        FreemarkerUtil.generator(toPath+"Test.java", data);
+    }
+
+    private static String getGeneratorPath() throws DocumentException {
         SAXReader reader = new SAXReader();
         Map<String, String> map = new HashMap<>();
         map.put("pom", "http://maven.apache.org/POM/4.0.0");
@@ -23,11 +44,6 @@ public class ServerGenerator {
         Document document = reader.read(pomPath);
         Node node = document.selectSingleNode("//pom:configurationFile");
         System.out.println(node.getText());
-
-
-//        FreemarkerUtil.initConfig("test.ftl");
-//        Map<String, Object> data = new HashMap<>();
-//        data.put("domain","Test");
-//        FreemarkerUtil.generator(toPath+"Test.java", data);
+        return node.getText();
     }
 }
