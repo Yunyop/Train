2734e999a04a3b19cb9ba9201a7f633e0b64c7fa
Yunyop <Yunyop.pyc@gmail.com>
2024-07-06 15:16:10 +0800

    8.2集成dom4j。读取pom.xml,得到当前持久层的xml文件

--- generator/src/main/java/com/yun/train/server/ServerGenerator.java
+++ generator/src/main/java/com/yun/train/server/ServerGenerator.java
@@ -1,22 +1,33 @@
 package com.yun.train.server;
 
-import com.yun.train.util.FreemarkerUtil;
-import freemarker.template.TemplateException;
+import org.dom4j.Document;
+import org.dom4j.Node;
+import org.dom4j.io.SAXReader;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ServerGenerator {
     static String toPath="generator/src/main/java/com/yun/train/test/";
+    static String pomPath="generator/pom.xml";
     static {
         new File(toPath).mkdir();
     }
-    public static void main(String[] args) throws IOException, TemplateException {
-        FreemarkerUtil.initConfig("test.ftl");
-        Map<String, Object> data = new HashMap<>();
-        data.put("domain","Test");
-        FreemarkerUtil.generator(toPath+"Test.java", data);
+    public static void main(String[] args) throws Exception{
+//        读取xml
+        SAXReader reader = new SAXReader();
+        Map<String, String> map = new HashMap<>();
+        map.put("pom", "http://maven.apache.org/POM/4.0.0");
+        reader.getDocumentFactory().setXPathNamespaceURIs(map);
+        Document document = reader.read(pomPath);
+        Node node = document.selectSingleNode("//pom:configurationFile");
+        System.out.println(node.getText());
+
+
+//        FreemarkerUtil.initConfig("test.ftl");
+//        Map<String, Object> data = new HashMap<>();
+//        data.put("domain","Test");
+//        FreemarkerUtil.generator(toPath+"Test.java", data);
     }
 }
