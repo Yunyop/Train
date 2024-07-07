c811628f006a72741a53ff61a58cbedc0dac534a
Yunyop <Yunyop.pyc@gmail.com>
2024-07-07 15:46:24 +0800

    8.3使用PassengerService制作模版，生成service成功

---
+++ generator/src/main/java/com/yun/train/ftl/service.ftl
@@ -0,0 +1,74 @@
+package com.yun.train.service;
+
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.date.DateTime;
+import cn.hutool.core.util.ObjectUtil;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.yun.train.context.LoginMemberContext;
+import com.yun.train.domain.${Domain};
+import com.yun.train.domain.${Domain}Example;
+import com.yun.train.mapper.${Domain}Mapper;
+import com.yun.train.req.${Domain}QueryReq;
+import com.yun.train.req.${Domain}SaveReq;
+import com.yun.train.resp.PageResp;
+import com.yun.train.resp.${Domain}QueryResp;
+import com.yun.train.util.SnowUtil;
+import jakarta.annotation.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ${Domain}Service {
+
+    private static final Logger LOGGER= LoggerFactory.getLogger(${Domain}Service.class);
+
+
+    @Resource
+    private ${Domain}Mapper ${domain}Mapper;
+    public void save(${Domain}SaveReq req) {
+        DateTime now = DateTime.now();
+        ${Domain} ${domain} = BeanUtil.copyProperties(req, ${Domain}.class);
+        if (ObjectUtil.isNull(${domain}.getId())) {
+            ${domain}.setMemberId(LoginMemberContext.getId());
+            ${domain}.setId(SnowUtil.getSnowflakeNextId());
+            ${domain}.setCreateTime(now);
+            ${domain}.setUpdateTime(now);
+            ${domain}Mapper.insert(${domain});
+        } else {
+            ${domain}.setUpdateTime(now);
+            ${domain}Mapper.updateByPrimaryKey(${domain});
+        }
+    }
+    public PageResp<${Domain}QueryResp> queryList(${Domain}QueryReq req){
+        ${Domain}Example ${domain}Example = new ${Domain}Example();
+        ${domain}Example.setOrderByClause("id desc");
+        ${Domain}Example.Criteria criteria = ${domain}Example.createCriteria();
+        if (ObjectUtil.isNotNull(req.getMemberId())) {
+            criteria.andMemberIdEqualTo(req.getMemberId());
+        }
+        LOGGER.info("查询页码：{}",req.getPage());
+        LOGGER.info("每页条数：{}",req.getSize());
+        PageHelper.startPage(req.getPage(),req.getSize());
+        List<${Domain}> ${domain}List = ${domain}Mapper.selectByExample(${domain}Example);
+        PageInfo<${Domain}> pageInfo = new PageInfo<>(${domain}List);
+
+        LOGGER.info("总行数：{}",pageInfo.getTotal());
+        LOGGER.info("总页数：{}",pageInfo.getPages());
+
+        List<${Domain}QueryResp> list = BeanUtil.copyToList(${domain}List, ${Domain}QueryResp.class);
+
+        PageResp<${Domain}QueryResp> pageResp = new PageResp<>();
+
+        pageResp.setList(list);
+
+        pageResp.setTotal(pageInfo.getTotal());
+        return pageResp;
+    }
+    public void delete(long id) {
+        ${domain}Mapper.deleteByPrimaryKey(id);
+    }
+}
--- generator/src/main/java/com/yun/train/server/ServerGenerator.java
+++ generator/src/main/java/com/yun/train/server/ServerGenerator.java
@@ -1,5 +1,6 @@
 package com.yun.train.server;
 
+import com.yun.train.util.FreemarkerUtil;
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
 import org.dom4j.Node;
@@ -10,30 +11,52 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ServerGenerator {
-    static String toPath="generator/src/main/java/com/yun/train/test/";
+    static String servicePath ="member/src/main/java/com/yun/train/service/";
     static String pomPath="generator/pom.xml";
     static {
-        new File(toPath).mkdir();
+        new File(servicePath).mkdir();
     }
     public static void main(String[] args) throws Exception{
-//        读取xml
+//        读取xml获取mybatis-generator
         String generatorPath = getGeneratorPath();
 
+//        比如generatoe-config-member.xml,得到module=member
+        String module = generatorPath.replace("[module]/src/main/resources/generator-config-","").replace(".xml","");
+        System.out.println("module:"+module);
 
-//        读xml里面的节点
+        servicePath=servicePath.replace("[module]",module);
+        System.out.println("servicePath:"+servicePath);
+
+
+//        读xml里面的节点table
 
         Document document = new SAXReader().read("generator/" + generatorPath);
         Node table = document.selectSingleNode("//table");
         System.out.println(table);
         Node tableName = table.selectSingleNode("@tableName");
-        Node domainObject = table.selectSingleNode("@domainObjectName");
-        System.out.println(tableName.getText()+"/"+domainObject.getText());
+        Node domainObjectName = table.selectSingleNode("@domainObjectName");
+        System.out.println(tableName.getText()+"/"+domainObjectName.getText());
 
+        // 示例：表名 Yunyop_test
+        // Domain = YunyopTest
+        String Domain = domainObjectName.getText();
+        // domain = yunyopTest
+        String domain = Domain.substring(0, 1).toLowerCase() + Domain.substring(1);
+        // do_main = yunyop-test
+        String do_main = tableName.getText().replaceAll("_", "-");
+//        组装参数
+        Map<String,Object> param = new HashMap<>();
+        param.put("Domain",Domain);
+        param.put("domain",domain);
+        param.put("do_main",do_main);
+        System.out.println("组装参数："+param);
+        // 表中文名
+//        String tableNameCn = DbUtil.getTableComment(tableName.getText());
+//        List<Field> fieldList = DbUtil.getColumnByTableName(tableName.getText());
+//        Set<String> typeSet = getJavaTypes(fieldList);
 
-//        FreemarkerUtil.initConfig("test.ftl");
-//        Map<String, Object> data = new HashMap<>();
-//        data.put("domain","Test");
-//        FreemarkerUtil.generator(toPath+"Test.java", data);
+        FreemarkerUtil.initConfig("service.ftl");
+        FreemarkerUtil.generator(servicePath+Domain+"Service.java",param);
     }
 
     private static String getGeneratorPath() throws DocumentException {
