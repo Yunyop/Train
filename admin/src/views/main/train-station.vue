d3fe0953dd470bf2192bf45f37df9799f98c5015
Yunyop <Yunyop.pyc@gmail.com>
2024-07-24 21:18:21 +0800

    9.13制作车次下拉框组件

--- admin/src/views/main/train-station.vue
+++ admin/src/views/main/train-station.vue
@@ -28,12 +28,7 @@
            ok-text="确认" cancel-text="取消">
     <a-form :model="trainStation" :label-col="{span: 4}" :wrapper-col="{ span: 20 }">
       <a-form-item label="车次编号">
-        <a-select v-model:value="trainStation.trainCode" show-search
-                  :filter-option="filterTrainCodeOption">
-          <a-select-option v-for="item in trains" :key="item.code" :value="item.code" :lable="item.code+item.start+item.end">
-            {{item.code}}|{{item.start}}~{{item.end}}
-          </a-select-option>
-        </a-select>
+        <TrainSelectView v-model="trainStation.trainCode"></TrainSelectView>
       </a-form-item>
       <a-form-item label="站序">
         <a-input v-model:value="trainStation.index" />
@@ -65,9 +60,11 @@ import {defineComponent, ref, onMounted, watch} from 'vue';
 import {notification} from "ant-design-vue";
 import axios from "axios";
 import {pinyin} from "pinyin-pro";
+import TrainSelectView  from "@/components/train-select.vue";
 
 export default defineComponent({
   name: "train-station-view",
+  components:{TrainSelectView},
   setup() {
     const visible = ref(false);
     let trainStation = ref({
@@ -227,9 +224,6 @@ export default defineComponent({
     const trains=ref([])
 
     // 查询所有的车次，用于车次下拉框
-
-
-
     const queryTrainCode = () => {
       axios.get("/business/admin/train/query-all").then((response)=>{
         let data=response.data;
