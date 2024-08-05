c3a090be984a34015894bd6f497f91897fffded1
Yunyop <Yunyop.pyc@gmail.com>
2024-08-05 13:19:34 +0800

    10.2增加batch定时调度模块

---
+++ batch/src/main/java/com/yun/train/config/BatchApplication.java
@@ -0,0 +1,32 @@
+package com.yun.train.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@SpringBootApplication
+@ComponentScan("com.yun")
+@MapperScan("com.yun.train.*.mapper")
+public class BatchApplication {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(BatchApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(BatchApplication.class);
+		try {
+			Environment env = app.run(args).getEnvironment();
+			LOGGER.info("启动成功");
+			LOGGER.info("地址：\thttp://127.0.0.1:{}{}/hello", env.getProperty("server.port"), env.getProperty("server.servlet.context-path"));
+
+		}
+		catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+}
---
+++ batch/src/main/java/com/yun/train/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.yun.train.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/hello")
+    public String toString() {
+        return "hello world! Batch";
+    }
+}
