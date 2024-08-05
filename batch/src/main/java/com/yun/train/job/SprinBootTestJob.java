3fbc79b02683330bc0a07c763eae5fb6b953ca75
Yunyop <Yunyop.pyc@gmail.com>
2024-08-05 15:27:09 +0800

    10.6禁止任务并发执行

--- batch/src/main/java/com/yun/train/job/SprinBootTestJob.java
+++ batch/src/main/java/com/yun/train/job/SprinBootTestJob.java
@@ -1,18 +1,18 @@
-package com.yun.train.job;
-
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
-
-//适合单体应用，不适合集群
-// 没法实时更改定时任务状态和策略
-
-@EnableScheduling
-@Component
-public class SprinBootTestJob {
-    @Scheduled(cron = "0/5 * * * * ?")
-    protected void test(){
-//        增加分布式锁
-        System.out.println("SprinBootTestJob Test");
-    }
-}
+//package com.yun.train.job;
+//
+//import org.springframework.scheduling.annotation.EnableScheduling;
+//import org.springframework.scheduling.annotation.Scheduled;
+//import org.springframework.stereotype.Component;
+//
+////适合单体应用，不适合集群
+//// 没法实时更改定时任务状态和策略
+//
+//@EnableScheduling
+//@Component
+//public class SprinBootTestJob {
+//    @Scheduled(cron = "0/5 * * * * ?")
+//    protected void test(){
+////        增加分布式锁
+//        System.out.println("SprinBootTestJob Test");
+//    }
+//}
--- batch/src/main/java/com/yun/train/job/TestJob.java
+++ batch/src/main/java/com/yun/train/job/TestJob.java
@@ -1,13 +1,20 @@
 package com.yun.train.job;
 
+import org.quartz.DisallowConcurrentExecution;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
-
+@DisallowConcurrentExecution
 public class TestJob implements Job {
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-        System.out.println("TestJob Test");
+        System.out.println("TestJob Test开始\n");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("TestJob Test结束");
 
     }
 }
