84eee818fbf1aa08b595d2816ea8e745279494c1
Yunyop <Yunyop.pyc@gmail.com>
2024-08-05 15:15:58 +0800

    10.5集成quartz，和sprinboot自带的定时任务

---
+++ batch/src/main/java/com/yun/train/job/TestJob.java
@@ -0,0 +1,13 @@
+package com.yun.train.job;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+public class TestJob implements Job {
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        System.out.println("TestJob Test");
+
+    }
+}
