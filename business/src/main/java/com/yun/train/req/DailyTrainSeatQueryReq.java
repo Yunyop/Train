2af32e4287ba3b5e0221c8a8f17278afba23641d
Yunyop <Yunyop.pyc@gmail.com>
2024-08-12 14:58:50 +0800

    11.6修改每日座位页面，增加车次查询条件，修改列号的显示

--- business/src/main/java/com/yun/train/req/DailyTrainSeatQueryReq.java
+++ business/src/main/java/com/yun/train/req/DailyTrainSeatQueryReq.java
@@ -4,9 +4,21 @@ import com.yun.train.req.PageReq;
 
 public class DailyTrainSeatQueryReq extends PageReq {
 
+    private String trainCode;
+
+    public String getTrainCode() {
+        return trainCode;
+    }
+
+    public void setTrainCode(String trainCode) {
+        this.trainCode = trainCode;
+    }
+
     @Override
     public String toString() {
-        return "DailyTrainSeatQueryReq{" +
-                "} " + super.toString();
+        final StringBuffer sb = new StringBuffer("DailyTrainSeatQueryReq{");
+        sb.append("trainCode='").append(trainCode).append('\'');
+        sb.append('}');
+        return sb.toString();
     }
 }
--- business/src/main/java/com/yun/train/service/DailyTrainSeatService.java
+++ business/src/main/java/com/yun/train/service/DailyTrainSeatService.java
@@ -43,8 +43,11 @@ public class DailyTrainSeatService {
     }
     public PageResp<DailyTrainSeatQueryResp> queryList(DailyTrainSeatQueryReq req){
         DailyTrainSeatExample dailyTrainSeatExample = new DailyTrainSeatExample();
-        dailyTrainSeatExample.setOrderByClause("id desc");
+        dailyTrainSeatExample.setOrderByClause("train_code asc, carriage_index asc, carriage_seat_index asc");
         DailyTrainSeatExample.Criteria criteria = dailyTrainSeatExample.createCriteria();
+        if (ObjectUtil.isNotEmpty(req.getTrainCode())){
+            criteria.andTrainCodeEqualTo(req.getTrainCode());
+        }
 
         LOGGER.info("查询页码：{}",req.getPage());
         LOGGER.info("每页条数：{}",req.getSize());
