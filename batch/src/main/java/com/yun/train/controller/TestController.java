b7e36ffe3e21c97e83ada799816b20a5169c7cbb
Yunyop <Yunyop.pyc@gmail.com>
2024-08-13 16:20:57 +0800

    11.8集成openfeign实现服务间调用。给每个应用增加应用命，可搭配配置中心使用

--- batch/src/main/java/com/yun/train/controller/TestController.java
+++ batch/src/main/java/com/yun/train/controller/TestController.java
@@ -1,12 +1,22 @@
 package com.yun.train.controller;
 
+import com.yun.train.feign.BusinessFeign;
+import jakarta.annotation.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    private static final Logger log = LoggerFactory.getLogger(TestController.class);
+    @Resource
+    BusinessFeign businessFeign;
+    
     @GetMapping("/hello")
-    public String toString() {
-        return "hello world! Batch";
+    public String hello() {
+        String businessHello = businessFeign.hello();
+        log.info(businessHello);
+        return "hello world! Batch!";
     }
 }
