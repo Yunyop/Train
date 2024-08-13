3a7a1f3385f615df9f9b3d1ca74a8095aeebf2ff
Yunyop <Yunyop.pyc@gmail.com>
2024-08-13 14:45:51 +0800

    11.7增加生成每日车次数据定时任务

---
+++ batch/src/main/java/com/yun/train/job/DailyTrainJob.java
@@ -0,0 +1,25 @@
+package com.yun.train.job;
+
+import cn.hutool.core.util.RandomUtil;
+import org.quartz.DisallowConcurrentExecution;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+
+@DisallowConcurrentExecution
+public class DailyTrainJob implements Job {
+    private static final Logger log = LoggerFactory.getLogger(DailyTrainJob.class);
+
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        MDC.put("LOG_ID",System.currentTimeMillis()+ RandomUtil.randomString(3));
+
+        DailyTrainJob.log.info("生成每日车次数据开始");
+
+        DailyTrainJob.log.info("生成每日车次数据结束");
+
+    }
+}
