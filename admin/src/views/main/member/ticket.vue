d26864dc33885ed87212aefd62c4ef7144edda5b
Yunyop <Yunyop.pyc@gmail.com>
2024-11-15 16:48:31 +0800

    12.22修改会员车票页面的展示

--- admin/src/views/main/member/ticket.vue
+++ admin/src/views/main/member/ticket.vue
@@ -15,7 +15,7 @@
       </template>
       <template v-else-if="column.dataIndex === 'col'">
         <span v-for="item in SEAT_ARRAY" :key="item.code">
-          <span v-if="item.code === record.col">
+          <span v-if="item.code === record.col&&item.type===record.seatType">
             {{item.desc}}
           </span>
         </span>
@@ -68,16 +68,7 @@ export default defineComponent({
     });
     let loading = ref(false);
     const columns = [
-    {
-      title: '会员id',
-      dataIndex: 'memberId',
-      key: 'memberId',
-    },
-    {
-      title: '乘客id',
-      dataIndex: 'passengerId',
-      key: 'passengerId',
-    },
+
     {
       title: '乘客姓名',
       dataIndex: 'passengerName',
