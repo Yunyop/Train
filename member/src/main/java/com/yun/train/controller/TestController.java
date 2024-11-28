bffee48f7e114e13a68414343e6dfca1f84d3cc3
Yunyop <Yunyop.pyc@gmail.com>
2024-11-28 15:49:37 +0800

    13.4 读取配置文件的配置

--- member/src/main/java/com/yun/train/controller/TestController.java
+++ member/src/main/java/com/yun/train/controller/TestController.java
@@ -1,12 +1,18 @@
 package com.yun.train.controller;
 
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+
+    @Value("${test.nacos}")
+    private String testNacos;
+
     @GetMapping("/hello")
-    public String toString() {
-        return "hello world";
+    public String hello() {
+        return String.format("Hello %s!", testNacos);
     }
 }
