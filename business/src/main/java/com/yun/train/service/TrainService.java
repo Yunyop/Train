f577143be74bfe354601b43710031a9a77234f53
Yunyop <Yunyop.pyc@gmail.com>
2024-12-19 13:08:47 +0800

    14.2 演示mybatis的一级缓存

--- business/src/main/java/com/yun/train/service/TrainService.java
+++ business/src/main/java/com/yun/train/service/TrainService.java
@@ -22,6 +22,7 @@ import jakarta.annotation.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -95,8 +96,11 @@ public class TrainService {
     }
 
     //查询所有车站
+    @Transactional
     public List<TrainQueryResp> queryAll(){
         List<Train> trainList = selectAll();
+//        LOGGER.info("再查一次");
+//        trainList = selectAll();
         return BeanUtil.copyToList(trainList, TrainQueryResp.class);
     }
 
