e47d7ce9547720b6e7adfb604039fd3e7bf758a0
Yunyop <Yunyop.pyc@gmail.com>
2025-01-07 18:41:55 +0800

    15.6 配置seata+nacos，分布式测试成功

--- business/src/main/java/com/yun/train/service/AfterConfirmOrderService.java
+++ business/src/main/java/com/yun/train/service/AfterConfirmOrderService.java
@@ -149,9 +149,10 @@ public class AfterConfirmOrderService {
 
 
 //            模拟调用方出现异常
-            if (1==1){
-                throw new Exception("测试异常");
-            }
+            Thread.sleep(10000);
+//            if (1==1){
+//                throw new Exception("测试异常");
+//            }
         }
 
     }
--- member/src/main/java/com/yun/train/service/TicketService.java
+++ member/src/main/java/com/yun/train/service/TicketService.java
@@ -39,9 +39,9 @@ public class TicketService {
             ticket.setUpdateTime(now);
             ticketMapper.insert(ticket);
 //            模拟被调用方出现异常
-        if (1==1){
-            throw new Exception("测试异常11");
-        }
+//        if (1==1){
+//            throw new Exception("测试异常11");
+//        }
     }
     public PageResp<TicketQueryResp> queryList(TicketQueryReq req){
         TicketExample ticketExample = new TicketExample();
