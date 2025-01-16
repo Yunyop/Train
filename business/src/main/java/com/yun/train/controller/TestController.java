4af03fb9797230132e44a9a2722326a6e04f11c2
Yunyop <Yunyop.pyc@gmail.com>
2025-01-16 16:31:44 +0800

    17.8 Sentinel限流不同的流控效果演示-WarmUp&排队等待

--- business/src/main/java/com/yun/train/controller/TestController.java
+++ business/src/main/java/com/yun/train/controller/TestController.java
@@ -1,12 +1,15 @@
 package com.yun.train.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@SentinelResource("hello")
 @RestController
 public class TestController {
     @GetMapping("/hello")
-    public String toString() {
+    public String hello() throws InterruptedException {
+        Thread.sleep(500);
         return "hello world! Business";
     }
 }
