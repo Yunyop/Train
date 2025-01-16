787d14133f2ac79589c3f20b2ac7b017d114a0a3
Yunyop <Yunyop.pyc@gmail.com>
2025-01-16 17:12:04 +0800

    17.9 Sentinel限流不同的流控效果演示-关联&链路等待

--- business/src/main/java/com/yun/train/controller/TestController.java
+++ business/src/main/java/com/yun/train/controller/TestController.java
@@ -1,15 +1,29 @@
 package com.yun.train.controller;
 
 import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.yun.train.service.TestService;
+import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SentinelResource("hello")
 @RestController
 public class TestController {
+
+    @Resource
+    private TestService testService;
+    @SentinelResource("hello")
     @GetMapping("/hello")
     public String hello() throws InterruptedException {
-        Thread.sleep(500);
+
+        testService.hello2();
         return "hello world! Business";
+    }
+
+    @SentinelResource("hello1")
+    @GetMapping("/hello1")
+    public String hello1() throws InterruptedException {
+
+        testService.hello2();
+        return "hello world! Business1";
     }
 }
