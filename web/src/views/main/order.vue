6f3a116d530240ef196cf7bd3c273f592c31fa3d
Yunyop <Yunyop.pyc@gmail.com>
2025-01-20 17:06:21 +0800

    18.8 增加第一层验证码削弱瞬时高峰

--- web/src/views/main/order.vue
+++ web/src/views/main/order.vue
@@ -54,7 +54,7 @@
   <a-modal v-model:visible="visible" title="请核对以下信息"
            style="top :50px;width: 800px"
            ok-text="确认" cancel-text="取消"
-           @ok="showImageCodeModal">
+           @ok="showFirstImageCodeModal">
     <div class="order-tickets">
       <a-row class="order-tickets-header" v-if="tickets.length>0">
         <a-col :span="3">乘客</a-col>
@@ -102,10 +102,13 @@
 <!--      最终选座：{{chooseSeatObj}}-->
     </div>
   </a-modal>
-  <!--      验证码-->
+  <!--      第二层验证码 后段-->
     <a-modal v-model:visible="imageCodeModalVisible" :title="null" :footer="null" :closable="false"
              style="top: 50px; width: 400px">
-    <p style="text-align: center ;font-weight: bold;font-size: 18px">使用验证码削弱瞬时高峰</p>
+    <p style="text-align: center ;font-weight: bold;font-size: 18px">
+      使用服务端验证码削弱瞬时高峰<br/>
+      防止机器人刷票
+    </p>
     <p>
       <a-input v-model:value="imageCode" placeholder="图片验证码">
         <template #suffix>
@@ -115,7 +118,22 @@
     </p>
     <a-button type="primary" danger block @click="handleOk">请输入验证码后开始购票</a-button>
   </a-modal>
-  <br>
+  <!-- 第一层验证码 纯前端 -->
+  <a-modal v-model:visible="firstImageCodeModalVisible" :title="null" :footer="null" :closable="false"
+           style="top: 50px; width: 400px">
+    <p style="text-align: center; font-weight: bold; font-size: 18px">
+      使用纯前端验证码削弱瞬时高峰<br/>
+      减小后端验证码接口的压力
+    </p>
+    <p>
+      <a-input v-model:value="firstImageCodeTarget" placeholder="验证码">
+        <template #suffix>
+          {{firstImageCodeSourceA}} + {{firstImageCodeSourceB}}
+        </template>
+      </a-input>
+    </p>
+    <a-button type="primary" danger block @click="validFirstImageCode">提交验证码</a-button>
+  </a-modal>
 </template>
 <script >
 
@@ -377,6 +395,42 @@ export default defineComponent({
       loadImageCode();
       imageCodeModalVisible.value=true;
     };
+
+    const firstImageCodeSourceA = ref();
+    const firstImageCodeSourceB = ref();
+    const firstImageCodeTarget = ref();
+    const firstImageCodeModalVisible = ref();
+
+    /**
+     * 加载第一层验证码
+     */
+    const loadFirstImageCode = () => {
+      // 获取1~10的数：Math.floor(Math.random()*10 + 1)
+      firstImageCodeSourceA.value = Math.floor(Math.random()*10 + 1) + 10;
+      firstImageCodeSourceB.value = Math.floor(Math.random()*10 + 1) + 20;
+    };
+
+    /**
+     * 显示第一层验证码弹出框
+     */
+    const showFirstImageCodeModal = () => {
+      loadFirstImageCode();
+      firstImageCodeModalVisible.value = true;
+    };
+
+    /**
+     * 校验第一层验证码
+     */
+    const validFirstImageCode = () => {
+      if (parseInt(firstImageCodeTarget.value) === parseInt(firstImageCodeSourceA.value + firstImageCodeSourceB.value)) {
+        // 第一层验证通过
+        firstImageCodeModalVisible.value = false;
+        showImageCodeModal();
+      } else {
+        notification.error({description: '验证码错误'});
+      }
+    };
+
     onMounted(() => {
       handelQueryPassenger();
     })
@@ -401,6 +455,12 @@ export default defineComponent({
       imageCode,
       showImageCodeModal,
       loadImageCode,
+      validFirstImageCode,
+      firstImageCodeSourceA,
+      firstImageCodeSourceB,
+      firstImageCodeTarget,
+      firstImageCodeModalVisible,
+      showFirstImageCodeModal,
     };
   },
 });
